18c609374ee5aa41b04e7ef0b1c5e9edd2792e59
Abdul Wahab <abdulwahab@gmail.com>
2023-10-01 14:37:13 +0500

    Cal Blog Todo

---
+++ Blog-web/next.config.mjs
@@ -0,0 +1,35 @@
+import withPlaiceholder from '@plaiceholder/next';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.notion.so',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3-us-west-2.amazonaws.com',
+      },
+    ],
+  },
+
+  // suppress keyv warning
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.ContextReplacementPlugin(/\/keyv\//, (data) => {
+        delete data.dependencies[0].critical;
+        return data;
+      })
+    );
+
+    return config;
+  },
+};
+
+export default withPlaiceholder(nextConfig)
